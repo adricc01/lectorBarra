5715bba6654ea68b7547af98ec132873b760de52
adricc01 <adominguez@cae3076.com>
2017-03-30 11:57:14 -0600

    Lector de codigos de barras

--- js/API/barcodeScanner.js
+++ js/API/barcodeScanner.js
@@ -13,7 +13,7 @@ var mc = {
 				  "preferFrontCamera" : false, // iOS and Android 
 				  "showFlipCameraButton" : true, // iOS and Android 
 				  "prompt" : "Coloque el codigo en el area especificada", // supported on Android only 
-				  //"formats" : "QR_CODE,DATA_MATRIX,UPC_E,UPC_A,EAN_8,EAN_13,CODE_128,CODE_39,CODE_93,CODABAR,ITF,RSS14,RSS_EXPANDED,PDF_417", // default: all but PDF_417 and RSS_EXPANDED 
+				  "formats" : "QR_CODE,RSS_EXPANDED,PDF_417",//"QR_CODE,DATA_MATRIX,UPC_E,UPC_A,EAN_8,EAN_13,CODE_128,CODE_39,CODE_93,CODABAR,ITF,RSS14,RSS_EXPANDED,PDF_417", // default: all but PDF_417 and RSS_EXPANDED 
 				  "orientation" : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device 
 				  "showTorchButton" : true, // iOS and Android 
 				  "torchOn": false, // Android, launch with the torch switched on (if available) 
