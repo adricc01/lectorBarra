f6d6f9931bffbcca4eb760798b1eea5b7511d063
adricc01 <adominguez@cae3076.com>
2017-04-03 14:41:17 -0500

    Agregando Info de red

--- js/API/barcodeScanner.js
+++ js/API/barcodeScanner.js
@@ -5,9 +5,10 @@ var mc = {
 				  if(result.text != ""){
 					  alert("Datos Obtenidos\n" +
 							"Result: " + result.text);
-					  fn.enviarRegistro(result.text);
 					  if(networkInfo.estaConectado() == false){
 						  alert("No existe conexion a internet");
+						 }else{
+						  fn.enviarRegistro(result.text);
 					  }
 				  }
 			  },
@@ -17,7 +18,7 @@ var mc = {
 			{
 				"preferFrontCamera" : false, // iOS and Android 
 				"showFlipCameraButton" : true, // iOS and Android 
-				"prompt" : "Coloque el código en el área especificada \\n Corporativo Aduanal Especializado S.C.", // supported on Android only 
+				"prompt" : "Coloque el código en el área especificada", // supported on Android only 
 				"formats" : "QR_CODE,RSS_EXPANDED,PDF_417",//"QR_CODE,DATA_MATRIX,UPC_E,UPC_A,EAN_8,EAN_13,CODE_128,CODE_39,CODE_93,CODABAR,ITF,RSS14,RSS_EXPANDED,PDF_417", // default: all but PDF_417 and RSS_EXPANDED 
 				//"orientation" : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device 
 				"showTorchButton" : true, // iOS and Android 
